c908ae16d82db63994179e947d577082fc74c26d
lawalTheWest <lwltjdn@gmail.com>
2025-01-31 09:56:35 +0100

    .ts file converted to .js for testing using the html file

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -3,8 +3,9 @@ var director1 = {
     lastName: 'Lawal',
     fullTimeEmployee: true,
     location: 'Osun',
-    numberOfReports: 17
+    numberOfReports: 17,
 };
+/***** Use Case for director interface ******/
 console.log(director1);
 var teacher_1 = {
     firstName: 'Lawal',
@@ -20,5 +21,29 @@ var teacher_2 = {
     yearsOfExperience: 5,
     location: 'Abuja',
 };
+/**** Use case for teacher interface ****/
 console.log(teacher_1);
 console.log(teacher_2);
+var printTeacher = function (firstName, lastName) {
+    return "".concat(firstName.charAt(0), ". ").concat(lastName);
+};
+/***** Use case ******/
+console.log(printTeacher("John", "Doe"));
+var StudentClass = /** @class */ (function () {
+    function StudentClass(_a) {
+        var firstName = _a.firstName, lastName = _a.lastName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return "Currently Working";
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
+/********* Use case *********/
+var std1 = new StudentClass({ firstName: 'Smart', lastName: 'Smith' });
+console.log(std1.displayName());
+console.log(std1.workOnHomework());
